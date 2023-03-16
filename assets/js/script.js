7a131c9974cad717232a517d8c78cb1c6bcdb6db
BuzWrecks <123795096+BuzWrecks@users.noreply.github.com>
2023-03-16 16:28:33 +1100

    calendar past/present/future colors working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,46 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+// Document ready function - hold all code until load
+$(document).ready(function () {
+// Display current time and date, updating once per second to ensure minute update is accurate.
+    $(function () {
+        function displayTime() {
+            let rightNow = dayjs().format('dddd, DD MMMM YYYY [at] HH:mm');
+            $('#time-display').text(rightNow);
+    // Timer interval for displayTime update        
+            let timerInterval = setInterval(function() {
+                displayTime();
+            }, 1000)};
+            displayTime();
+    // Clear Calendar button - clears fields and clears local storage
+            $("#clearCalendarBtn").click(function (event) {
+                event.preventDefault;
+                $("textarea").val("");
+                localStorage.clear();
+            });
+    // define current hour using dayjs(), compares timeBlock with current hour
+                let currentHour = dayjs().format("HH");
+                $(".timeBlock").each(function () {
+                    var timeBlock = $(this).attr("id").split("-")[1];
+    // if time block is equal to current hour, add "present", if else, past, if else, future               
+                    if (currentHour === timeBlock) {
+                        $(this).addClass("present");
+                    } else if (currentHour <= timeBlock) {
+                        $(this).removeClass("present");
+                        $(this).addClass("future");
+                    } else if (currentHour >= timeBlock) {
+                        $(this).removeClass("future");
+                        $(this).removeClass("present");
+                        $(this).addClass("past");
+                    }
+                    });
+
+    });
+  
+})
+
+var textArea = window.localStorage.textArea(0);
+
+localstorage
+    
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -9,16 +48,8 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+   
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
